082e4e9a08996b87b7ccd6c5a50be575ee5da016
Levi Currit <levi.currit@gmail.com>
2023-01-20 09:54:40 -0700

    Astro w/TailwindCSS

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+  ],
+});
